4775780e1eaa4f626f32ff744893cc7da6c322c2
choewy <choewy32@gmail.com>
2024-03-02 13:46:02 +0900

    feature/usePathname hook needs 'use client'

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,16 +1,27 @@
+'use client';
+
 import Link from 'next/link';
 
 import { PageStaticPath } from '../constants';
+import { usePathname } from 'next/navigation';
 
 export default function Navigation() {
+  const pathname = usePathname();
+
+  const hereIcon = (path: PageStaticPath) => (pathname === path ? '👈' : '');
+
   return (
     <nav>
       <ul>
         <li>
-          <Link href={PageStaticPath.Home}>Home</Link>
+          <Link href={PageStaticPath.Home}>
+            Home {hereIcon(PageStaticPath.Home)}
+          </Link>
         </li>
         <li>
-          <Link href={PageStaticPath.About}>About</Link>
+          <Link href={PageStaticPath.About}>
+            About {hereIcon(PageStaticPath.About)}
+          </Link>
         </li>
       </ul>
     </nav>
